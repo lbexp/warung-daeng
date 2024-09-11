09a099eb103b9e954066eeda97e2dbac9e385931
lbexp <fiqarapr@gmail.com>
2024-09-12 00:49:33 +0800

    feat: api products get all and get by id

--- api/src/controllers/product.controller.ts
+++ api/src/controllers/product.controller.ts
@@ -34,16 +34,24 @@ export class ProductController {
   }
 
   @Get()
-  getAll(@Query() query: PaginationRequest): string {
-    const products = this.productService.getAll(query);
+  async getAll(@Query() query: PaginationRequest): Promise<string> {
+    const result = await this.productService.getAll(query);
 
-    return `This action returns all product ${JSON.stringify(products)}`;
+    if (!result.success) {
+      return result.error;
+    }
+
+    return `This action returns all product ${JSON.stringify(result.products)}`;
   }
 
   @Get(':id')
-  getById(@Param() params: { id: string }): string {
-    const product = this.productService.getById(parseInt(params.id, 10));
+  async getById(@Param() params: { id: string }): Promise<string> {
+    const result = await this.productService.getById(parseInt(params.id, 10));
 
-    return `This action returns specific product ${JSON.stringify(product)}`;
+    if (!result.success) {
+      return result.error;
+    }
+
+    return `This action returns specific product ${JSON.stringify(result.product)}`;
   }
 }
--- api/src/service/product.service.ts
+++ api/src/service/product.service.ts
@@ -67,17 +67,85 @@ export class ProductService {
     }
   }
 
-  async getAll(pagination: PaginationRequest): Promise<Product[]> {
-    console.log(pagination);
-    const res = await this.conn.query('SELECT * FROM products');
+  async getAll(
+    pagination: PaginationRequest,
+  ): Promise<{ success: boolean; products: Product[]; error: string }> {
+    try {
+      const resultProducts = await this.conn.query(
+        'SELECT p.id, p.category_id, c.name AS category_name, p.sku, p.name, p.description, p.weight, p.width, p.length, p.height, p.image, p.price FROM products p LEFT JOIN categories c ON p.category_id = c.id LIMIT $1 OFFSET $2',
+        [pagination.limit, pagination.limit * (pagination.page - 1)],
+      );
 
-    return res.rows;
+      if (resultProducts.rowCount === 0) {
+        throw 'Error not found';
+      }
+
+      const products = resultProducts.rows.map<Product>((rawProduct) => ({
+        id: rawProduct.id,
+        categoryId: rawProduct.category_id,
+        categoryName: rawProduct.category_name,
+        description: rawProduct.description,
+        height: rawProduct.height,
+        image: rawProduct.image,
+        length: rawProduct.length,
+        name: rawProduct.name,
+        price: rawProduct.price,
+        sku: rawProduct.sku,
+        weight: rawProduct.weight,
+        width: rawProduct.width,
+      }));
+
+      return {
+        success: true,
+        products,
+        error: '',
+      };
+    } catch (error) {
+      return {
+        success: false,
+        products: [],
+        error: error.message || '',
+      };
+    }
   }
 
-  async getById(id: number): Promise<Product> {
-    const result = await this.conn.query(
-      `SELECT * FROM products WHERE id === ${id}`,
-    );
-    return result.rows[0];
+  async getById(
+    id: number,
+  ): Promise<{ success: boolean; product: Product | null; error: string }> {
+    try {
+      const resultProduct = await this.conn.query(
+        `SELECT p.id, p.category_id, c.name AS category_name, p.sku, p.name, p.description, p.weight, p.width, p.length, p.height, p.image, p.price FROM products p LEFT JOIN categories c ON p.category_id = c.id WHERE p.id = $1`,
+        [id],
+      );
+
+      if (resultProduct.rowCount === 0) {
+        throw 'Error not found';
+      }
+
+      return {
+        success: true,
+        product: {
+          id: resultProduct.rows[0].id,
+          categoryId: resultProduct.rows[0].category_id,
+          categoryName: resultProduct.rows[0].category_name,
+          description: resultProduct.rows[0].description,
+          height: resultProduct.rows[0].height,
+          image: resultProduct.rows[0].image,
+          length: resultProduct.rows[0].length,
+          name: resultProduct.rows[0].name,
+          price: resultProduct.rows[0].price,
+          sku: resultProduct.rows[0].sku,
+          weight: resultProduct.rows[0].weight,
+          width: resultProduct.rows[0].width,
+        },
+        error: '',
+      };
+    } catch (error) {
+      return {
+        success: false,
+        product: null,
+        error: error.message || '',
+      };
+    }
   }
 }
