5c6bafd1625a9f6419a09f1a5bb21062f8052ff4
lbexp <fiqarapr@gmail.com>
2024-09-13 00:57:52 +0800

    feat: create base client app

---
+++ client/src/components/Header/HeaderComp.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <header class="wrapper">
+    <RouterLink to="/">
+      <h1>Warung Daeng</h1>
+    </RouterLink>
+
+    <nav>
+      <RouterLink to="/login">Login</RouterLink>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 12px;
+  margin-bottom: solid 2px;
+}
+</style>
--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts"></script>
 
 <template>
-  <main>Home</main>
+  <main>Login</main>
 </template>
